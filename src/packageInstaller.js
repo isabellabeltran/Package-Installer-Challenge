63bc4d1a047641fc73c758821f9ec4f3f0ad3fab
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-08-28 01:01:28 -0400

    Install sinon and tested parameter type

--- src/packageInstaller.js
+++ src/packageInstaller.js
@@ -1,15 +1,15 @@
 module.exports = (pkgs) => {
   let packages = pkgs; 
   if (!Array.isArray(packages)) {
+    console.log('from inside',packages)
     throw 'It should be an array of packages';
   }
   for (let i = 0; i < packages.length; i++) {
-    console.log(packages, packages[i]);
-    if (packages[i] !== 'string') {
+    if (typeof packages[i] !== 'string') {
       throw 'Each package should be a string'; 
     }
   }
-  return []; 
+  return ['jellp']; 
 }
 
 
--- test/packageInstaller.spec.js
+++ test/packageInstaller.spec.js
@@ -1,10 +1,18 @@
 const chai = require('chai'); 
 const expect = chai.expect;
+const sinon = require('sinon'); 
 const packageInstaller = require('../src/packageInstaller.js'); 
 
 describe('Package Installer', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
   it('should be a function', () => {
     expect(packageInstaller).to.be.a('function'); 
   });
-
+  it('should only accept an array as an argument', () => {
+    let verifyParameter = sinon.spy(packageInstaller);
+    packageInstaller(['hello', 'hello', 'hello']);
+    expect(verifyParameter.args).to.be.an('array');
+  }); 
 });
