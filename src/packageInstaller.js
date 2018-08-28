101ee8f9cea67b5ffde5bfd4c9c6743fa21f774f
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-08-28 12:25:32 -0400

    Refactor for separation of concerns and created test to verify the creation of the dictionary

--- src/packageInstaller.js
+++ src/packageInstaller.js
@@ -1,15 +1,34 @@
-module.exports = (pkgs) => {
-  let packages = pkgs; 
-  if (!Array.isArray(packages)) {
-    console.log('from inside',packages)
+
+const PackageInstaller = (packagesList) => {  
+  if (!Array.isArray(packagesList)) {
     throw 'It should be an array of packages';
   }
-  for (let i = 0; i < packages.length; i++) {
-    if (typeof packages[i] !== 'string') {
+  for (let i = 0; i < packagesList.length; i++) {
+    if (typeof packagesList[i] !== 'string') {
       throw 'Each package should be a string'; 
     }
   }
-  return ['jellp']; 
+  CreateDictionary(packagesList); 
 }
 
+const CreateDictionary = (packages) => {
+  let dictionary = {};
+  for (let i = 0; i < packages.length; i++) {
+    let packagePair = packages[i].split(':'); 
+    let package = packagePair[0].trim(); 
+    let dependency = packagePair[1].trim(); 
+    if (package.length === 0) {
+      throw 'Missing package'; 
+    }
+    dictionary[package] = [];
+    if (dependency.length > 0) {
+      dictionary[package].push(dependency); 
+    }
+  }
+  return dictionary;
+}
 
+module.exports = {
+  PackageInstaller, 
+  CreateDictionary
+}
--- test/packageInstaller.spec.js
+++ test/packageInstaller.spec.js
@@ -1,18 +1,33 @@
 const chai = require('chai'); 
 const expect = chai.expect;
 const sinon = require('sinon'); 
-const packageInstaller = require('../src/packageInstaller.js'); 
+const {PackageInstaller, CreateDictionary} = require('../src/packageInstaller.js'); 
+
 
 describe('Package Installer', () => {
   afterEach(() => {
     sinon.restore();
   });
   it('should be a function', () => {
-    expect(packageInstaller).to.be.a('function'); 
+    expect(PackageInstaller).to.be.a('function'); 
   });
   it('should only accept an array as an argument', () => {
-    let verifyParameter = sinon.spy(packageInstaller);
-    packageInstaller(['hello', 'hello', 'hello']);
+    let verifyParameter = sinon.spy(PackageInstaller);
+    PackageInstaller([]);
     expect(verifyParameter.args).to.be.an('array');
   }); 
+  describe('createDictionary', () => {
+    it('should be a function', () => {
+      expect(CreateDictionary).to.be.a('function'); 
+    });
+    it('should return an object', () => {
+      let result = { KittenService: [],
+        Leetmeme: [ 'Cyberportal' ],
+        Cyberportal: [ 'Ice' ],
+        CamelCaser: [ 'KittenService' ],
+        Fraudstream: [ 'Leetmeme' ],
+        Ice: [] };
+      expect(CreateDictionary(['KittenService: ','Leetmeme: Cyberportal','Cyberportal: Ice','CamelCaser:KittenService','Fraudstream: Leetmeme','Ice: '])).to.deep.equal(result); 
+    });
+  });
 });
