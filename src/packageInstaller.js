bd6f1fe00030462c22c54190206a185e02056e07
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-08-28 16:04:47 -0400

    Created check cycles function

--- src/packageInstaller.js
+++ src/packageInstaller.js
@@ -1,5 +1,6 @@
 
 const PackageInstaller = (packagesList) => {  
+  let result = []; 
   if (!Array.isArray(packagesList)) {
     throw 'It should be an array of packages';
   }
@@ -8,7 +9,12 @@ const PackageInstaller = (packagesList) => {
       throw 'Each package should be a string'; 
     }
   }
-  CreateDictionary(packagesList); 
+  const dictionary = CreateDictionary(packagesList); 
+  const verifyCycles = CheckCycles(dictionary); 
+  if (verifyCycles) {
+    return 'It should not contain cycles between packages';
+  }
+  
 }
 
 const CreateDictionary = (packages) => {
@@ -28,7 +34,49 @@ const CreateDictionary = (packages) => {
   return dictionary;
 }
 
+const CheckCycles = (dictionary) => {
+  let visited = {}; 
+  let result = false; 
+
+  const verifyCycle = (package, dependencies) => {
+    if (visited[package]) {
+      return; 
+    }
+    dependencies.push(package); 
+    let dependency = dictionary[package]; 
+    for(let j = 0; j < dependency.length; j++) {
+      if (dependencies.indexOf(dependency[j]) >= 0) {
+        result = true; 
+        return result;  
+      }
+      verifyCycle(dependency[j], dependencies);
+    }
+    visited[package] = true; 
+  }
+
+  let packages = Object.keys(dictionary); 
+  for (let i = 0; i < packages.length; i++) {
+    verifyCycle(packages[i],[]);
+  }
+  return result; 
+}
+
+console.log(PackageInstaller(['KittenService: ','Leetmeme: Cyberportal','Cyberportal: Ice','CamelCaser:KittenService','Fraudstream: Leetmeme','Ice: ']));
+
 module.exports = {
   PackageInstaller, 
   CreateDictionary
 }
+
+  /**
+   * I => object 
+   * O => boolean 
+   * Pseudocode 
+   * visitedPackages = keep a reference for each package visited []
+   * for each package name in the dictionary 
+   * Check that the package on visitedPackages is not repeated
+   *  if is repeated return true
+   *  else push into visitedPackages and check it's dependencies 
+   * 
+   * 
+  */
