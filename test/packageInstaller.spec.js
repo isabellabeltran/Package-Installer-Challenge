fe99f79d9400e851b0016ea88f23f5dd6ac7e591
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-08-28 20:39:30 -0400

    Review and add tests for check cycles and package installer

--- test/packageInstaller.spec.js
+++ test/packageInstaller.spec.js
@@ -1,26 +1,21 @@
 const chai = require('chai'); 
 const expect = chai.expect;
-const sinon = require('sinon'); 
-const {PackageInstaller, CreateDictionary} = require('../src/packageInstaller.js'); 
+const {PackageInstaller, CreateDictionary, CheckCycles} = require('../src/packageInstaller.js'); 
 
 
 describe('Package Installer', () => {
-  afterEach(() => {
-    sinon.restore();
-  });
   it('should be a function', () => {
     expect(PackageInstaller).to.be.a('function'); 
   });
-  it('should only accept an array as an argument', () => {
-    let verifyParameter = sinon.spy(PackageInstaller);
-    PackageInstaller([]);
-    expect(verifyParameter.args).to.be.an('array');
-  }); 
+  it('should return a string with the packages sorted in order to install ', () => {
+    let verifyOutput = PackageInstaller(['KittenService: ','Leetmeme: Cyberportal','Cyberportal: Ice','CamelCaser:KittenService','Fraudstream: Leetmeme','Ice: ']);
+    expect(verifyOutput).to.be.a('string');
+  });
   describe('createDictionary', () => {
     it('should be a function', () => {
       expect(CreateDictionary).to.be.a('function'); 
     });
-    it('should return an object', () => {
+    it('should return an object with packages as properties', () => {
       let result = { KittenService: [],
         Leetmeme: [ 'Cyberportal' ],
         Cyberportal: [ 'Ice' ],
@@ -30,4 +25,27 @@ describe('Package Installer', () => {
       expect(CreateDictionary(['KittenService: ','Leetmeme: Cyberportal','Cyberportal: Ice','CamelCaser:KittenService','Fraudstream: Leetmeme','Ice: '])).to.deep.equal(result); 
     });
   });
+  describe('Check cycle', () => {
+    it('should be a function', () => {
+      expect(CheckCycles).to.be.a('function'); 
+    });
+    it('should return a string if it not contains a cycle', () => {
+      let cycle = CheckCycles({ KittenService: [],
+        Leetmeme: [ 'Cyberportal' ],
+        Cyberportal: [ 'Ice' ],
+        CamelCaser: [ 'KittenService' ],
+        Fraudstream: [ 'Leetmeme' ],
+        Ice: [] });
+      expect(cycle).to.be.a('string'); 
+    });
+    it('should return an array when there is a cycle', () => {
+      let cycle = CheckCycles({ KittenService: [],
+        Leetmeme: [ 'Cyberportal' ],
+        Cyberportal: [ 'Ice' ],
+        CamelCaser: [ 'KittenService' ],
+        Fraudstream: [],
+        Ice: [ 'Leetmeme' ] });
+      expect(cycle).to.be.an('array'); 
+    });
+  }); 
 });
